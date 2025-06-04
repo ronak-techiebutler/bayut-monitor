0e08c4b93d93baa4fa9a20f90af209fba57c474c
ronak-techiebutler <ronak.pipalva@techiebutler.com>
2025-06-04 15:54:44 +0530

    fix monitor.js

--- monitor.js
+++ monitor.js
@@ -1,5 +1,5 @@
-import { runMonitor } from "./services/monitorCore";
-import { parseSitemap } from "./services/parseSitemap";
+import { runMonitor } from "./services/monitorCore.js";
+import { parseSitemap } from "./services/parseSitemap.js";
 
 const urls = parseSitemap();
 await runMonitor(urls);
