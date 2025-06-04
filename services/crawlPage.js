714d2339ea5b78df92faaae5219c4d5a9716ec13
ronak-techiebutler <ronak.pipalva@techiebutler.com>
2025-06-04 10:09:06 +0530

    fix crawlPage

--- services/crawlPage.js
+++ services/crawlPage.js
@@ -1,33 +1,42 @@
 import puppeteer from "puppeteer";
 
 export const crawlPage = async (url) => {
-  const browser = await puppeteer.launch({ headless: true });
-  // const browser = await puppeteer.launch({
-  //   executablePath: "/snap/bin/chromium",
-  //   headless: "new",
-  //   args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  // });
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle2" });
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    // const browser = await puppeteer.launch({
+    //   executablePath: "/snap/bin/chromium",
+    //   headless: "new",
+    //   args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    // });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle2" });
 
-  const html = await page.content();
+    const html = await page.content();
 
-  const localStorageData = await page.evaluate(() => {
-    const obj = {};
-    for (let i = 0; i < window.localStorage.length; i++) {
-      const key = window.localStorage.key(i);
-      obj[key] = window.localStorage.getItem(key);
-    }
-    return obj;
-  });
+    const localStorageData = await page.evaluate(() => {
+      const obj = {};
+      for (let i = 0; i < window.localStorage.length; i++) {
+        const key = window.localStorage.key(i);
+        obj[key] = window.localStorage.getItem(key);
+      }
+      return obj;
+    });
 
-  const links = await page.$$eval("a", (anchors) => anchors.map((a) => a.href));
+    const links = await page.$$eval("a", (anchors) =>
+      anchors.map((a) => a.href)
+    );
 
-  await browser.close();
+    await browser.close();
 
-  return {
-    html,
-    localStorage: localStorageData, // safe object
-    links,
-  };
+    return {
+      html,
+      localStorage: localStorageData, // safe object
+      links,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
