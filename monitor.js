42afae451087d74ae7a57503fdd3dddf614b4e42
ronak-techiebutler <ronak.pipalva@techiebutler.com>
2025-06-04 11:23:44 +0530

    add cron job

--- monitor.js
+++ monitor.js
@@ -1,6 +1,16 @@
 import { runMonitor } from "./services/monitorCore.js";
 import { parseSitemap } from "./services/parseSitemap.js";
+import cron from "node-cron";
 
-const urls = parseSitemap();
-
-await runMonitor(urls);
+// Schedule to run every day at 12:00 AM UTC
+cron.schedule(
+  "30 11 * * *",
+  async () => {
+    console.log("⏰ Running daily monitor at 11:30 AM UTC...");
+    const urls = parseSitemap();
+    await runMonitor(urls);
+  },
+  {
+    timezone: "UTC",
+  }
+);
