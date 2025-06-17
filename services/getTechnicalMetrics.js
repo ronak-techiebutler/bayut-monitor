d194bbce4222be2e612cc7e4181343dbc65a237a
ronak-techiebutler <ronak.pipalva@techiebutler.com>
2025-06-17 13:08:16 +0530

    fix getTechnicalMetrics

--- services/getTechnicalMetrics.js
+++ services/getTechnicalMetrics.js
@@ -1,19 +1,25 @@
-import { launch } from "chrome-launcher";
+import { chromium } from "playwright";
 import lighthouse from "lighthouse";
 
 export const getTechnicalMetrics = async (url) => {
-  const chrome = await launch({ chromeFlags: ["--headless"] });
+  const browser = await chromium.launch({
+    headless: true,
+    args: ["--remote-debugging-port=9222"],
+  });
+
   const result = await lighthouse(url, {
-    port: chrome.port,
+    port: 9222,
     output: "json",
     logLevel: "info",
   });
+
+  await browser.close();
 
   const { audits } = result.lhr;
 
   return {
     TTFB: audits["server-response-time"]?.numericValue,
     LCP: audits["largest-contentful-paint"]?.numericValue,
-    INP: audits["interactive"]?.numericValue,
+    INP: audits["experimental-interaction-to-next-paint"]?.numericValue,
   };
 };
