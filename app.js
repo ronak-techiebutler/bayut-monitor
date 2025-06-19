ec3341aaa074de690b96e37e5adac23c9c8f49ba
ronak-techiebutler <ronak.pipalva@techiebutler.com>
2025-06-19 12:08:10 +0530

    add log for check utc time

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cron from "node-cron";
 import { runMonitor } from "./services/monitorCore.js";
 import { parseSitemap } from "./services/parseSitemap.js";
+import moment from "moment/moment.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Bayut-monitor is ready to go" });
 });
 
+console.log(moment());
 //Schedule to run every day at 12:00 AM UTC
 cron.schedule("0 0 4 6 *", async () => {
   console.log("⏰ Running daily monitor at 12:00 AM UTC...");
